f8601f2408e95e9d0379848879c15869c1088d20
Martin Dimitrov <martin.dimi97@gmail.com>
2024-09-28 17:38:18 +0100

    fuck eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -12,6 +12,7 @@ const config = {
   ],
   rules: {
     "@typescript-eslint/array-type": "off",
+    "@typescript-eslint/prefer-nullish-coalescing": "off",
     "@typescript-eslint/consistent-type-definitions": "off",
     "@typescript-eslint/no-empty-object-type": "off",
     "@typescript-eslint/consistent-type-imports": [
